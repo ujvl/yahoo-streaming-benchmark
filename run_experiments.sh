5742b4551ccc0a8a6c21a01809eadd976eb7e774
Ionel Gog <gogionel@gmail.com>
2017-12-01 17:43:57 +0000

    Added script to run experiments.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,241 +1,96 @@
 #!/bin/bash
 
-# cp conf/benchmarkConf-10000k-1000ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-1000ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-1000ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-1000ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-100ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-100ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-100ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-100ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-10ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-10ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-10ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-10ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-200ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-200ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-200ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-200ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-500ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-500ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-500ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-500ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-50ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-50ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-10000k-50ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-10000k-50ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-1000ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-1000ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-1000ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-1000ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-100ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-100ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-100ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-100ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-10ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-10ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-10ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-10ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-200ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-200ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-200ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-200ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-500ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-500ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-500ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-500ms-6cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-50ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-50ms-12cores-flink-1.txt
-# sleep 10
-# # ----------------------------
-# cp conf/benchmarkConf-20000k-50ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-20000k-50ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-1000ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-1000ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-1000ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-1000ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-100ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-100ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-100ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-100ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-10ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-10ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-10ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-10ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-200ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-200ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-200ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-200ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-500ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-500ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-500ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-500ms-6cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-50ms-12cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-50ms-12cores-flink-1.txt
-# sleep 10
-# #----------------------------
-# cp conf/benchmarkConf-4000k-50ms-6cores.yaml conf/benchmarkConf.yaml
-# ./stream-bench.sh FLINK_TEST
-# mv data/latencies.txt results/latencies-4000k-50ms-6cores-flink-1.txt
-# sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-1000ms-failure-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-10ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-10ms-3cores-checkpoint-1000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-2000ms-failure-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-10ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-10ms-3cores-checkpoint-2000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-2000ms-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-10ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-10ms-3cores-checkpoint-5000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-1000ms-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-1000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-5000ms-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-2000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-5000ms-failure-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-5000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-2000ms-exactly-once-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-4000k-10ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-10ms-3cores-checkpoint-1000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-2000ms-exactly-once-failure-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-4000k-10ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-10ms-3cores-checkpoint-2000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-1000ms-exactly-once-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-4000k-10ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-10ms-3cores-checkpoint-5000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-1000ms-exactly-once-failure-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-4000k-50ms-3cores-checkpoint-1000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-50ms-3cores-checkpoint-1000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-5000ms-exactly-once-flink-$i.txt
+  sleep 10
+done
+
 #----------------------------
-cp conf/benchmarkConf-4000k-50ms-3cores-checkpoint-2000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-50ms-3cores-checkpoint-2000ms-failure-flink-1.txt
-sleep 10
-#----------------------------
-cp conf/benchmarkConf-4000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
-./stream-bench.sh FLINK_TEST
-mv data/latencies.txt results/latencies-4000k-50ms-3cores-checkpoint-5000ms-failure-flink-1.txt
-sleep 10
+for i in `seq 1 10` ; do
+  cp conf/benchmarkConf-10000k-50ms-3cores-checkpoint-5000ms.yaml conf/benchmarkConf.yaml
+  ./stream-bench.sh FLINK_TEST_FAILURE
+  mv data/latencies.txt results/latencies-10000k-50ms-3cores-checkpoint-5000ms-exactly-once-failure-flink-$i.txt
+  sleep 10
+done
